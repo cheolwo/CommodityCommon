3bc1bdfc6907cdc0e24fad72139511186c060c07
cheolwo <powerqkrcjfd@gmail.com>
2024-05-07 17:56:18 +0900

    .

---
+++ 상품Common/상품Common/상품.cs
@@ -0,0 +1,22 @@
+﻿namespace 상품Common
+{
+    public enum 품목구분
+    {
+        원재료,
+        부재료,
+        제품,
+        반제품,
+        상품,
+        무형상품
+    }
+    public class 상품
+    {
+        public string 품목코드 { get; set; }
+        public string 품목명 { get; set; }
+        public string 그룹명 { get; set; }
+        public string 규격명 { get; set; }
+        public string 바코드 { get; set; }
+        public 품목구분 품목구분 { get; set; }  // Enum 타입으로 변경
+        public string 이미지 { get; set; }  // 이미지 URL 또는 경로를 저장
+    }
+}
---
+++ 상품Common/상품Common/상품DbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace 상품Common
+{
+    public class 상품DbContext : DbContext
+    {
+        public DbSet<상품> 상품 { get; set; }
+
+        public 상품DbContext(DbContextOptions<상품DbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new 상품Configuration());
+        }
+    }
+    public class 상품Configuration : IEntityTypeConfiguration<상품>
+    {
+        public void Configure(EntityTypeBuilder<상품> builder)
+        {
+            builder.HasKey(x => x.품목코드);
+            builder.Property(x => x.품목명).IsRequired().HasMaxLength(100);
+            builder.Property(x => x.그룹명).HasMaxLength(50);
+            builder.Property(x => x.규격명).HasMaxLength(50);
+            builder.Property(x => x.바코드).HasMaxLength(20);
+            builder.Property(x => x.품목구분).IsRequired();  // Enum 처리
+            builder.Property(x => x.이미지).HasMaxLength(255);  // 이미지 파일 경로 또는 URL
+        }
+    }
+}
